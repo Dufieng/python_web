7050acc097b3829ec7d43ee15440e5d403b25710
GitWine <linuxgit050@gmail.com>
2025-03-13 18:40:23 +0100

    Initial commit

---
+++ data type.py
@@ -0,0 +1,31 @@
+# Datove typy
+## STRING = text
+pozdrav = "Ahoj, Svete"
+print(pozdrav)
+print(type(pozdrav))
+
+## INTEGER = cele cislo 
+rok = 2025
+print(rok)
+print(type(rok))
+
+## FLOAT = desatinne cislo
+prumerna_znamka = 3.5
+print(prumerna_znamka)
+print(type(prumerna_znamka))
+
+## BOOLEAN = True/False
+Doucovani = True
+if (Doucovani == True):
+  print("Od 18h budes jeste delat skolu")
+else:
+  print("Muzes si zahrat na PC")
+print(type(Doucovani))
+
+## LIST = seznam
+Znamky_z_testu = [1,2,3,4,5]
+Predmety_ve_skole = ["Matematika", "Fyzika", "Chemie", "Biologie", "Anglictina"]
+print(Znamky_z_testu)
+print(Predmety_ve_skole)
+print(type(Znamky_z_testu))
+print(type(Predmety_ve_skole)) 
---
+++ lekce1.py
@@ -0,0 +1,13 @@
+# Tady uzivatel zada znamky
+znamka1 = int(input("Zadej prvni znamku: "))
+znamka2 = int(input("Zadej druhou znamku: "))
+znamka3 = int(input("Zadej treti znamku: "))
+
+# Tady se vypocte prumer
+prumer = (znamka1 + znamka2 + znamka3) / 3
+
+# Tady vypiseme prumer
+print("Prumer znamek je: " + str(prumer)) 
+
+
+print(f"Prumer znamek: {prumer: .1f}, coz je super") 
